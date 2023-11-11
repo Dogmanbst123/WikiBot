20ef446e25281ccdcd507d811a9255d301b54db7
Dogmanbst123 <123bst0@gmail.com>
2023-11-11 00:05:10 -0800

    modification

--- commands/debug.js
+++ commands/debug.js
@@ -4,8 +4,6 @@ module.exports = {
 		.setName('debug')
 		.setDescription('do not use this command'),
 	async execute(interaction) {
-            let guilds = interaction.client.guilds.cache
-            console.log(guilds)
-            interaction.reply('logged');
+            interaction.reply('nothing');
 	},
 };
